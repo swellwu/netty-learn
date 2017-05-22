1cfab897e1ab563885df9566dbfb0edf3920a84e
swellwu <heng_0577@163.com>
2017-05-22 14:57:36 +0800

    netty 之 echo服务器

---
+++ src/main/java/com/Client/EchoClientHandler.java
@@ -0,0 +1,19 @@
+package com.Client;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * Created by wuxinjian on 2017/5/22.
+ */
+public class EchoClientHandler  extends SimpleChannelInboundHandler<String> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.err.println(msg);
+    }
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ src/main/java/com/server/EchoServer.java
@@ -0,0 +1,33 @@
+package com.server;
+
+import com.HandlerInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * Created by wuxinjian on 2017/5/21.
+ */
+public class EchoServer {
+
+    static final int PORT = 10011;
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new HandlerInitializer(new EchoServerHandler()));
+            b.bind(PORT).sync().channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+}
